94576891dc2efd63f309264b41e26f02b49f1ede
Tomi <tomi.holstila@gmail.com>
2018-10-04 14:44:45 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$wines = [
+  "producer" => "",
+  "name" => "",
+  "vintage" => "",
+  "grape" => [],
+  "region" => [
+    "country" => "",
+    "area" => "",
+    "terroir" => ""
+  ],
+  "description" => [
+    "body" => "",
+    "bouquet" => "",
+    "alcohol_content" => "",
+    "maturation" => "",
+    "comments" => "",
+  ];
+  "price" => "";
+]
